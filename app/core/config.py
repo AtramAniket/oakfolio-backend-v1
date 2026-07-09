5daeef13712b11026e2d4fb14015e893f09cb6f2
AtramAniket <aniket_atram@live.in>
2026-07-09 16:16:14 +0530

    Added configuration for API server

---
+++ app/core/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+
+	app_name: str = "Oakfolio API"
+	api_v1_prefix: str = "api/v1"
+	debug: bool = True
+
+	database_url: str
+	secret_key: str
+
+	model_config = SettingsConfigDict(
+		env_file=".env",
+		extra="ignore",
+	)
+
+settings = Settings()
