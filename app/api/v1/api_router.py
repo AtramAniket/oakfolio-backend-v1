3c5df2d98605bcf21cd6a54d2e4b76711ee8f570
AtramAniket <aniket_atram@live.in>
2026-07-09 17:08:07 +0530

    Renamed the routes to resolve import name conflict

--- app/api/v1/api_router.py
+++ app/api/v1/api_router.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 from app.modules.auth.router import auth_router
 
-api_router = APIRouter()
+api_v1_router = APIRouter()
 
-api_router.include_router(api_router)
+api_v1_router.include_router(auth_router)
