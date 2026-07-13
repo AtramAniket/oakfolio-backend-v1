d2ff4952acf8fd66b456951c3948e3ffdcd95110
AtramAniket <aniket_atram@live.in>
2026-07-13 14:56:22 +0530

    Fixed typo

--- app/db/session.py
+++ app/db/session.py
@@ -3,7 +3,7 @@ from app.core.config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(settings.DATABSE_URL, echo=settings.debug)
+engine = create_engine(settings.DATABASE_URL, echo=settings.debug)
 
 SessionLocal = sessionmaker(
 	bind=engine,
