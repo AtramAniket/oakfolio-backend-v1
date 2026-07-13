bee092aab0769ca327db74c1404e68907b81bebd
AtramAniket <aniket_atram@live.in>
2026-07-13 15:54:24 +0530

    Added default db url from settings

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from app.db.base import Base
+from app.core.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -26,6 +27,10 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.database_url,
+)
 
 
 def run_migrations_offline() -> None:
