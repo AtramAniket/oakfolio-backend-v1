03ec5049502a3866a10cc815edf7342af39c59bb
AtramAniket <aniket_atram@live.in>
2026-07-14 14:25:10 +0530

    Added register route

--- app/modules/auth/router.py
+++ app/modules/auth/router.py
@@ -1,3 +1,5 @@
+from app.modules.auth.schemas import RegisterRequest
+from app.api.v1.dependency import SessionDep
 from fastapi import APIRouter
 
 auth_router = APIRouter(prefix="/auth", tags=["Authentication"])
@@ -7,3 +9,7 @@ async def auth_test():
 	return {
 		"message":"Authentication router working"
 	}
+
+@auth_router.post("/register")
+async def register(payload: RegisterRequest,db: SessionDep):
+	pass
