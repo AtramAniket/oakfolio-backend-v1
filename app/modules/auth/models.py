c1e76955b32a01080ea12f07c9022e60507c6af3
AtramAniket <aniket_atram@live.in>
2026-07-15 15:27:32 +0530

    Created User model

--- app/modules/auth/models.py
+++ app/modules/auth/models.py
@@ -1,9 +1,10 @@
+from sqlalchemy import String, Text, Boolean, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import UUID as SQLAlchemyUUID
 from datetime import datetime
 from app.db.base import Base
 from uuid import UUID, uuid4
-from sqlalchemy import UUID as SQLAlchemyUUID
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Text, Boolean, DateTime, func
+
 
 class PendingRegistration(Base):
 	
@@ -35,5 +36,45 @@ class PendingRegistration(Base):
 	created_at: Mapped[datetime] = mapped_column(
 		DateTime(timezone=True),
 		server_default=func.now(),
+		nullable=False,
+	)
+
+
+class User(Base):
+
+	__tablename__ = "users"
+
+	id: Mapped[UUID] = mapped_column(
+		SQLAlchemyUUID(as_uuid=True),
+		primary_key=True,
+		default=uuid4,
+	)
+
+	email: Mapped[str] = mapped_column(
+		String(255),
+		nullable=False,
+		unique=True,
+	)
+
+	username: Mapped[str] = mapped_column(
+		String(100),
+		nullable=False,
+	)
+
+	password_hash: Mapped[str] = mapped_column(
+		Text,
+		nullable=True,
+	)
+
+	verified_at: Mapped[datetime] = mapped_column(
+		DateTime(timezone=True),
+		server_default=func.now(),
+		nullable=False,
+	)
+
+	updated_at: Mapped[datetime] = mapped_column(
+		DateTime(timezone=True),
+		server_default=func.now(),
+		onupdate=func.now(),
 		nullable=False,
 	)
