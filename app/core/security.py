ca2c04cfee292ada83202ff927bf3f7778396d0a
AtramAniket <aniket_atram@live.in>
2026-07-16 13:49:24 +0530

    Added password verification and hashing logic

--- app/core/security.py
+++ app/core/security.py
@@ -1,5 +1,11 @@
 import hashlib
 import secrets
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(
+	schemes=["bcrypt"],
+	deprecated="auto",
+)
 
 def generate_verification_token() -> str:
 	""" Function generates tokens for user email verification """
@@ -8,3 +14,9 @@ def generate_verification_token() -> str:
 def hash_verification_token(token: str) -> str:
 	""" Function hashes the token and returns encoded string """
 	return hashlib.sha256(token.encode("utf-8")).hexdigest()
+
+def hash_password(password: str) -> str:
+	return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+	return pwd_context.verify(plain_password, hashed_password)
