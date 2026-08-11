c01fdc16f02b0f4422df943daa8dd8e8116254ee
AtramAniket <aniket_atram@live.in>
2026-08-11 14:59:37 +0530

    Added schema for Stock Portfolio and Holdings

---
+++ app/modules/stocks/schema.py
@@ -0,0 +1,80 @@
+from app.modules.stocks.models import Exchange
+from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import Optional
+from decimal import Decimal
+from uuid import UUID
+
+
+
+# *********************************************
+# **************Holdings Schema****************
+# *********************************************
+
+class AddStockRequest(BaseModel):
+	company_name: str
+	ticker: str
+	exchange: Exchange
+	quantity: int = Field(gt=0)
+	buy_price: Decimal = Field(gt=0)
+	buy_date: datetime
+
+
+class AddStockResponse(BaseModel):
+	message: str
+	id: UUID
+	company_name: str
+	ticker: str
+
+
+class HoldingsResponse(BaseModel):
+	id: UUID
+	portfolio_id: UUID
+	company_name: str
+	exchange: Exchange
+	ticker: str
+	quantity: int = Field(gt=0)
+	buy_price: Decimal = Field(gt=0)
+	buy_date: datetime
+
+
+class GetHoldingsResponse(BaseModel):
+	holdings: list[HoldingsResponse]
+
+
+class DeleteHoldingsResponse(BaseModel):
+	message: str
+
+
+
+# *********************************************
+# *************Portfolio Schema****************
+# *********************************************
+
+class CreateNewPortfolioRequest(BaseModel):
+	name: str = Field(min_length=3)
+	description: Optional[str] = None
+
+
+class CreateNewPortfolioResponse(BaseModel):
+	message: str
+	id: UUID
+	name: str
+	description: Optional[str] = None
+
+
+class PortfolioResponse(BaseModel):
+	id: UUID
+	user_id: UUID
+	name: str
+	description: Optional[str] = None
+	holdings: list[HoldingsResponse]
+
+
+class GetAllPortfoliosResponse(BaseModel):
+	message: str
+	portfolios: list[PortfolioResponse]
+
+
+class DeletePortfolioResponse(BaseModel):
+	message: str
