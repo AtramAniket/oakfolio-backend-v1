758df25ac01ff68e9bd386541f5e4272ea8ae3b6
AtramAniket <aniket_atram@live.in>
2026-08-12 16:17:43 +0530

    Added routes for stocks and stock hilding and portfolios

--- app/modules/stocks/routes.py
+++ app/modules/stocks/routes.py
@@ -1,38 +1,46 @@
 from app.modules.stocks.schema import (
-	AddStockRequest,
-	AddStockResponse,
-	HoldingsResponse,
-	PortfolioResponse,
-	GetHoldingsResponse,
-	DeleteHoldingsResponse,
-	DeletePortfolioResponse,
-	GetAllPortfoliosResponse,
-	CreateNewPortfolioRequest,
 	CreateNewPortfolioResponse,
+	CreateNewPortfolioRequest,
+	GetAllPortfoliosResponse,
+	DeletePortfolioResponse,
+	DeleteHoldingsResponse,
+	GetHoldingsResponse,
+	PortfolioResponse,
+	HoldingsResponse,
+	AddStockResponse,
+	AddStockRequest,
 )
 
 from uuid import UUID
-from sqlalchemy import select
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, status
 from app.api.v1.dependency import SessionDep
-from fastapi import APIRouter, Depends, status
+from app.modules.stocks.dependency import UserDep
 import app.modules.stocks.service as stocks_service
-from app.modules.auth.models import User
-import app.modules.auth.services as auth_service
 
 
-api_v1_stocks_router = APIRouter(prefix="/stocks", tags=["Stocks"])
+api_v1_stocks_router = APIRouter(prefix="/stocks", tags=["Portfolios And Stock Holdings"])
 
-@api_v1_stocks_router.get("/portfolios", status_code=status.HTTP_200_OK)
-async def get_all_portfolios( db: SessionDep, current_user: User = Depends(auth_service.get_current_user)):
-	return await stocks_service.get_all_portfolios(db=db, current_user=current_user)
+# ***************************************************************************************************************************
+# ************************************************PORTFOLIO ROUTES***********************************************************
+# ***************************************************************************************************************************
+
+
+@api_v1_stocks_router.get("/portfolios", response_model=GetAllPortfoliosResponse, status_code=status.HTTP_200_OK)
+async def get_all_portfolios(
+	db: SessionDep,
+	current_user: UserDep):
+	
+	return await stocks_service.get_all_portfolios(
+		db=db,
+		current_user=current_user
+	)
 
 
 @api_v1_stocks_router.post("/portfolios", response_model=CreateNewPortfolioResponse, status_code=status.HTTP_201_CREATED)
 async def create_new_portfolio(
 	db: SessionDep,
-	payload: CreateNewPortfolioRequest,
-	current_user: User = Depends(auth_service.get_current_user)):
+	current_user: UserDep,
+	payload: CreateNewPortfolioRequest):
 	
 	return await stocks_service.create_new_portfolio(
 		db=db,
@@ -41,6 +49,102 @@ async def create_new_portfolio(
 	)
 
 
-@api_v1_stocks_router.delete("/portfolios/{portfolio_id}", response_model=DeleteHoldingsResponse)
-def delete_portfolio(portfolio_id: UUID, db:SessionDep):
-	pass
+@api_v1_stocks_router.put("/portfolios/{portfolio_id}", response_model=CreateNewPortfolioResponse, status_code=status.HTTP_200_OK)
+async def edit_portfolio(
+	db: SessionDep,
+	portfolio_id: UUID,
+	current_user: UserDep,
+	payload: CreateNewPortfolioRequest):
+	
+	return await stocks_service.edit_portfolio(
+		db=db,
+		payload=payload,
+		current_user=current_user,
+		portfolio_id=portfolio_id,
+	)
+
+
+@api_v1_stocks_router.delete("/portfolios/{portfolio_id}", response_model=DeletePortfolioResponse, status_code=status.HTTP_200_OK)
+async def delete_portfolio(
+	db: SessionDep,
+	portfolio_id: UUID,
+	current_user: UserDep):
+	
+	return await stocks_service.delete_portfolio(
+		db=db,
+		portfolio_id=portfolio_id,
+		current_user=current_user
+	)
+
+
+# ***************************************************************************************************************************
+# **************************************************PORTFOLIO ROUTES END*****************************************************
+# ***************************************************************************************************************************
+
+
+
+
+
+# ***************************************************************************************************************************
+# ***************************************************HOLDINGS ROUTES*********************************************************
+# ***************************************************************************************************************************
+
+
+@api_v1_stocks_router.get("/portfolios/{portfolio_id}/holdings", response_model=GetHoldingsResponse, status_code=status.HTTP_200_OK)
+async def get_holdings(
+	db: SessionDep,
+	portfolio_id: UUID,
+	current_user: UserDep):
+	
+	return await stocks_service.get_all_stock_holdings(
+		db=db,
+		portfolio_id=portfolio_id,
+		current_user=current_user,
+	)
+
+
+@api_v1_stocks_router.post("/portfolios/{portfolio_id}/holdings", response_model=AddStockResponse, status_code=status.HTTP_200_OK)
+async def add_stock_to_portfolio(
+	db: SessionDep,
+	portfolio_id: UUID,
+	current_user: UserDep,
+	payload: AddStockRequest):
+	
+	return await stocks_service.add_stock_to_portfolio(
+		db=db,
+		payload=payload,
+		portfolio_id=portfolio_id,
+		current_user=current_user,
+	)
+
+
+# TODO: CREATE EDIT ROUTE FOR STOCK HOLDING
+@api_v1_stocks_router.put("/portfolios/{portfolio_id}/holdings/{holding_id}", status_code=status.HTTP_501_NOT_IMPLEMENTED)
+def update_stock_in_portfolio(
+	db: SessionDep,
+	holding_id: UUID,
+	portfolio_id: UUID):
+	
+	return{
+		"message":"No implemented yet!"
+	}
+
+
+@api_v1_stocks_router.delete("/portfolios/{portfolio_id}/holdings/{holding_id}", response_model=DeleteHoldingsResponse, status_code=status.HTTP_200_OK)
+async def remove_stock_from_portfolio(
+	db: SessionDep,
+	holding_id: UUID,
+	portfolio_id: UUID,
+	current_user=UserDep):
+	
+	return await stocks_service.delete_stock_from_portfolio(
+		db=db,
+		holding_id=holding_id,
+		portfolio_id=portfolio_id,
+		current_user=current_user,
+		)
+
+
+# ***************************************************************************************************************************
+# ***************************************************HOLDINGS ROUTES END*****************************************************
+# ***************************************************************************************************************************
--- app/modules/stocks/service.py
+++ app/modules/stocks/service.py
@@ -1,25 +1,38 @@
 from app.modules.stocks.schema import (
-	AddStockRequest,
-	AddStockResponse,
-	HoldingsResponse,
-	PortfolioResponse,
-	GetHoldingsResponse,
-	DeleteHoldingsResponse,
-	DeletePortfolioResponse,
-	GetAllPortfoliosResponse,
-	CreateNewPortfolioRequest,
 	CreateNewPortfolioResponse,
+	CreateNewPortfolioRequest,
+	GetAllPortfoliosResponse,
+	DeletePortfolioResponse,
+	DeleteHoldingsResponse,
+	GetHoldingsResponse,
+	PortfolioResponse,
+	HoldingsResponse,
+	AddStockResponse,
+	AddStockRequest,
 )
 
-import uuid
+from uuid import UUID
 from datetime import datetime
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-from app.api.v1.dependency import SessionDep
 from app.modules.auth.models import User
 from fastapi import status, HTTPException
+from app.api.v1.dependency import SessionDep
 from app.modules.stocks.models import StockPortfolio, StockHolding
 
+not_found_exception = HTTPException(
+		status_code=status.HTTP_404_NOT_FOUND,
+		detail='Portfolio(s) not found',
+)
+
+stock_not_found_exception = HTTPException(
+		status_code=status.HTTP_404_NOT_FOUND,
+		detail='Stock not found',
+)
+
+# *****************************************************************
+# ***********************GET ALL PORTFOLIOS************************
+# *****************************************************************
 
 async def get_all_portfolios(db: SessionDep, current_user: User) -> GetAllPortfoliosResponse:
 
@@ -27,19 +40,30 @@ async def get_all_portfolios(db: SessionDep, current_user: User) -> GetAllPortfo
 		StockPortfolio.user_id == current_user.id
 	)
 
-	result = db.execute(statement).scalars().all()
+	result = db.execute(statement)
 
-	return GetAllPortfoliosResponse(
-		message="Data is available!",
-		portfolios=result,
+	portfolios = result.scalars().all()
+
+	if not portfolios:
+		return GetAllPortfoliosResponse(
+		portfolios=portfolios,
+		message="No portfolio available. Please add one.",
 	)
 
+	return GetAllPortfoliosResponse(
+		portfolios=portfolios,
+		message="Data is available!",
+	)
+
+# *****************************************************************
+# *********************CREATE NEW PORTFOLIO************************
+# *****************************************************************
 
 async def create_new_portfolio(payload: CreateNewPortfolioRequest, db:SessionDep, current_user: User) -> CreateNewPortfolioResponse:
 	
 	new_portfolio = StockPortfolio(
-		user_id = current_user.id,
 		name=payload.name,
+		user_id = current_user.id,
 		description=payload.description,
 	)
 
@@ -50,8 +74,191 @@ async def create_new_portfolio(payload: CreateNewPortfolioRequest, db:SessionDep
 	db.refresh(new_portfolio)
 
 	return CreateNewPortfolioResponse(
-		message="Portfolio Created Successfully",
 		id=new_portfolio.id,
 		name=new_portfolio.name,
-		description=new_portfolio.description
+		description=new_portfolio.description,
+		message="Portfolio Created Successfully",
 	)
+
+# *****************************************************************
+# ***************************EDIT PORTFOLIO************************
+# *****************************************************************
+
+async def edit_portfolio(portfolio_id: UUID, payload: CreateNewPortfolioRequest, db:SessionDep, current_user: User) -> CreateNewPortfolioResponse:
+	
+	statement = select(StockPortfolio).where(
+		StockPortfolio.id == portfolio_id,
+		StockPortfolio.user_id == current_user.id,
+	)
+
+	result = db.execute(statement)
+
+	portfolio = result.scalar_one_or_none()
+
+	if portfolio:
+		
+		updated_data = payload.model_dump(exclude_unset=True)
+
+		for field, value in updated_data.items():
+			setattr(portfolio, field, value)
+
+		db.commit()
+
+		db.refresh(portfolio)
+
+		return CreateNewPortfolioResponse(
+		id=portfolio.id,
+		name=portfolio.name,
+		description=portfolio.description,
+		message="Portfolio Updated Successfully",
+		)
+
+	else:
+		raise not_found_exception
+
+
+# *****************************************************************
+# ************************DELETE PORTFOLIO*************************
+# *****************************************************************
+
+async def delete_portfolio(portfolio_id: UUID, current_user: User, db: SessionDep) -> DeletePortfolioResponse:
+	
+	statement = select(StockPortfolio).where(
+		StockPortfolio.id == portfolio_id,
+		StockPortfolio.user_id == current_user.id,
+	)
+
+	result = db.execute(statement)
+
+	portfolio = result.scalar_one_or_none()
+
+	if portfolio:
+		
+		db.delete(portfolio)
+		
+		db.commit()
+
+
+		return DeletePortfolioResponse(
+			message="Portfolio deleted successfully"
+		)
+
+	else:
+		raise not_found_exception
+
+
+# *****************************************************************
+# *************************GET ALL HOLDINGS************************
+# *****************************************************************
+
+async def get_all_stock_holdings(db: SessionDep, portfolio_id: UUID, current_user: User) -> GetHoldingsResponse:
+
+	portfolio_statement = select(StockPortfolio).where(
+		StockPortfolio.id == portfolio_id,
+		StockPortfolio.user_id == current_user.id
+	)
+
+	portfolio_result = db.execute(portfolio_statement)
+
+	portfolio = portfolio_result.scalar_one_or_none()
+
+	if not portfolio:
+		raise not_found_exception
+
+
+	holdings_statement = select(StockHolding).where(
+		StockHolding.portfolio_id == portfolio_id
+	)
+
+	holdings_result = db.execute(holdings_statement)
+
+	holdings = holdings_result.scalars().all()
+
+	return GetHoldingsResponse(
+		holdings=holdings
+	)
+
+
+# *****************************************************************
+# *******************ADD STOCK TO STOCK PORTFOLIO******************
+# *****************************************************************
+async def add_stock_to_portfolio(db: SessionDep, payload: AddStockRequest, portfolio_id: UUID, current_user: User) -> AddStockResponse:
+	
+	portfolio_statement = select(StockPortfolio).where(
+		StockPortfolio.id == portfolio_id,
+		StockPortfolio.user_id == current_user.id
+	)
+
+	portfolio_result = db.execute(portfolio_statement)
+
+	portfolios = portfolio_result.scalar_one_or_none()
+
+	if not portfolios:
+		raise not_found_exception
+
+
+	new_stock = StockHolding(
+		ticker=payload.ticker,
+		portfolio_id=portfolio_id,
+		exchange=payload.exchange,
+		quantity=payload.quantity,
+		buy_date=payload.buy_date,
+		buy_price=payload.buy_price,
+		company_name=payload.company_name,
+	)
+
+	db.add(new_stock)
+
+	db.commit()
+
+	db.refresh(new_stock)
+
+	return AddStockResponse(
+		id=new_stock.id,
+		ticker=new_stock.ticker,
+		company_name=new_stock.company_name,
+		message="New stock added successfully"
+	)
+
+
+# *****************************************************************
+# *******************DELETE STOCK FROM PORTFOLIO*******************
+# *****************************************************************
+
+async def delete_stock_from_portfolio(db: SessionDep, holding_id: UUID, portfolio_id: UUID, current_user: User) -> DeleteHoldingsResponse:
+	
+	portfolio_statement = select(StockPortfolio).where(
+		StockPortfolio.id == portfolio_id,
+		StockPortfolio.user_id == current_user.id
+	)
+
+	portfolio_result = db.execute(portfolio_statement)
+
+	portfolio = portfolio_result.scalar_one_or_none()
+
+	if not portfolio:
+		raise not_found_exception
+
+
+	holding_statement = select(StockHolding).where(
+		StockHolding.id == holding_id,
+		StockHolding.portfolio_id == portfolio_id,
+	)
+
+	holding_result = db.execute(holding_statement)
+
+	holding = holding_result.scalar_one_or_none()
+
+	if holding:
+
+		db.delete(holding)
+
+		db.commit()
+
+		return DeleteHoldingsResponse(
+			message="Stock removed from portfolio"
+		)
+
+	else:
+		raise stock_not_found_exception
+
