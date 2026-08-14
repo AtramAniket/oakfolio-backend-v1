59ce4f14eb12f8b2957baaadee7fe223270d2870
AtramAniket <aniket_atram@live.in>
2026-08-14 15:02:47 +0530

    Added CORS middleware

--- app/main.py
+++ app/main.py
@@ -3,10 +3,22 @@ from fastapi import FastAPI
 from app.core.config import settings
 from app.api.v1.api_router import api_v1_router
 
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
 	title=settings.app_name,
 	debug=settings.debug,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://oakfolio-app.onrender.com",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
