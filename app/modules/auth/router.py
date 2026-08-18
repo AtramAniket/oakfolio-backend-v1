cc667157b23046eb6d18d73bdd7e19788bfc3265
AtramAniket <aniket_atram@live.in>
2026-08-18 16:02:20 +0530

    Added logout logic

--- app/modules/auth/router.py
+++ app/modules/auth/router.py
@@ -1,59 +1,130 @@
 from app.modules.auth.schemas import (
-	VerifyRegistrationTokenResponse,
-	VerifyRegistrationTokenRequest,
-	VerifyUserResponse,
-	VerifyUserRequest,
-	RegisterResponse,
-	RegisterRequest,
-	DeleteResponse,
-	DeleteRequest,
-	LoginResponse,
-	LoginRequest,
-	UserResponse,
-	UserRequest,
+    VerifyRegistrationTokenResponse,
+    VerifyRegistrationTokenRequest,
+    VerifyUserResponse,
+    VerifyUserRequest,
+    RegisterResponse,
+    RegisterRequest,
+    DeleteResponse,
+    DeleteRequest,
+    LoginResponse,
+    LoginRequest,
+    UserResponse,
+    UserRequest,
 )
-import app.modules.auth.services as auth_service 
-from app.api.v1.dependency import SessionDep
+
+from app.modules.auth.models import User, UserSession
+
+import app.modules.auth.services as auth_service
+
 from fastapi import APIRouter, Depends, status
 
-auth_router = APIRouter(prefix="/auth", tags=["Authentication"])
-
-@auth_router.get("/test")
-async def auth_test():
-	return {
-		"message":"Authentication router working"
-	}
+from app.api.v1.dependency import SessionDep
 
 
-
-@auth_router.post("/verify-registration-token", response_model=VerifyRegistrationTokenResponse)
-async def verify_registration_token(payload: VerifyRegistrationTokenRequest, db: SessionDep):
-	return await auth_service.verify_user_registration_token(db=db, payload=payload)
-
-
-@auth_router.post("/delete_pending_registration", response_model=DeleteResponse)
-async def delete_pending_user(payload: DeleteRequest,db: SessionDep):
-	return await auth_service.delete_pending_registration(db=db, payload=payload)
+auth_router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"],
+)
 
 
-@auth_router.post("/create_user", response_model=VerifyUserResponse)
-async def create_user(payload: VerifyUserRequest, db:SessionDep):
-	return await auth_service.verify_and_create_new_user(db=db, payload=payload)
+@auth_router.post(
+    "/verify-registration-token",
+    response_model=VerifyRegistrationTokenResponse,
+)
+async def verify_registration_token(
+    payload: VerifyRegistrationTokenRequest,
+    db: SessionDep,
+):
+    return await auth_service.verify_user_registration_token(
+        db=db,
+        payload=payload,
+    )
 
 
-@auth_router.post("/register", response_model=RegisterResponse, status_code=status.HTTP_201_CREATED)
-async def register(payload: RegisterRequest,db: SessionDep):
-	return await auth_service.register_user(db=db, payload=payload)
+@auth_router.post(
+    "/delete_pending_registration",
+    response_model=DeleteResponse,
+)
+async def delete_pending_user(
+    payload: DeleteRequest,
+    db: SessionDep,
+):
+    return await auth_service.delete_pending_registration(
+        db=db,
+        payload=payload,
+    )
+
+
+@auth_router.post(
+    "/create_user",
+    response_model=VerifyUserResponse,
+)
+async def create_user(
+    payload: VerifyUserRequest,
+    db: SessionDep,
+):
+    return await auth_service.verify_and_create_new_user(
+        db=db,
+        payload=payload,
+    )
+
+
+@auth_router.post(
+    "/register",
+    response_model=RegisterResponse,
+    status_code=status.HTTP_201_CREATED,
+)
+async def register(
+    payload: RegisterRequest,
+    db: SessionDep,
+):
+    return await auth_service.register_user(
+        db=db,
+        payload=payload,
+    )
+
 
 @auth_router.get("/me")
-async def current_user(current_user: UserResponse = Depends(auth_service.get_current_user)):
-	return UserResponse(
-		id=current_user.id,
-		email=current_user.email,
-		username=current_user.username,
-		verified_at=current_user.verified_at,
-	)
+async def current_user(
+    current_user: User = Depends(
+        auth_service.get_current_user
+    ),
+):
+    return UserResponse(
+        id=current_user.id,
+        email=current_user.email,
+        username=current_user.username,
+        verified_at=current_user.verified_at,
+    )
 
-@auth_router.post("/login", response_model=LoginResponse)
-async def login(payload: LoginRequest, db:SessionDep):
-	return await auth_service.login(db=db, payload=payload)
+
+@auth_router.post(
+    "/login",
+    response_model=LoginResponse,
+)
+async def login(
+    payload: LoginRequest,
+    db: SessionDep,
+):
+    return await auth_service.login(
+        db=db,
+        payload=payload,
+    )
+
+
+@auth_router.post("/logout")
+async def logout(
+    db: SessionDep,
+    current_session: UserSession = Depends(
+        auth_service.get_current_session
+    ),
+):
+    await auth_service.logout_user(
+        db=db,
+        current_session=current_session,
+    )
+
+    return {
+        "message": "Successfully logged out"
+    }
--- app/modules/auth/services.py
+++ app/modules/auth/services.py
@@ -1,323 +1,438 @@
 from app.modules.auth.schemas import (
-	VerifyRegistrationTokenResponse,
-	VerifyRegistrationTokenRequest,
-	VerifyUserResponse,
-	VerifyUserRequest,
-	RegisterResponse,
-	RegisterRequest,
-	DeleteResponse,
-	DeleteRequest,
-	LoginResponse,
-	LoginRequest,
-	UserResponse,
-	UserRequest,
+    VerifyRegistrationTokenResponse,
+    VerifyRegistrationTokenRequest,
+    VerifyUserResponse,
+    VerifyUserRequest,
+    RegisterResponse,
+    RegisterRequest,
+    DeleteResponse,
+    DeleteRequest,
+    LoginResponse,
+    LoginRequest,
+    UserResponse,
+    UserRequest,
 )
+
 from app.core.security import (
-	generate_verification_token,
-	generate_verification_link,
-	hash_verification_token,
-	create_access_token,
-	decode_access_token,
-	verify_password,
-	hash_password,
+    generate_verification_token,
+    generate_verification_link,
+    hash_verification_token,
+    create_access_token,
+    decode_access_token,
+    verify_password,
+    hash_password,
 )
-from app.modules.auth.models import PendingRegistration, User
+
+from app.modules.auth.models import (
+    PendingRegistration,
+    UserSession,
+    User,
+)
+
 from datetime import datetime, timezone, timedelta
+
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, ExpiredSignatureError
+
 from app.api.v1.dependency import SessionDep
-from fastapi import HTTPException, status
+
+from fastapi import HTTPException, status, Depends
+
 from sqlalchemy.orm import Session
 from sqlalchemy import select
-from fastapi import Depends
+
 from uuid import UUID
 
 from app.utils.mail_service import send_email_verification_mail
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
-
-
-async def login(db:Session, payload:LoginRequest) -> LoginResponse:
-	
-	# FInd user by email
-	find_user_statement = select(User).where(
-		User.email == payload.email
-	)
-
-	result = db.execute(find_user_statement)
-
-	user = result.scalar_one_or_none()
-
-	# If user is not present
-	if not user:
-		raise HTTPException(
-			status_code=status.HTTP_401_UNAUTHORIZED,
-			detail='Invalid email or password'
-		)
-	
-	# If password verification failed
-	password_match = verify_password(payload.password, user.password_hash)
-	if not password_match:
-		raise HTTPException(
-			status_code=status.HTTP_401_UNAUTHORIZED,
-			detail='Invalid email or password'
-		)
-	
-	# Generate JWT token
-	jwt_token = create_access_token(user.id)
-	return LoginResponse(
-		token_type="bearer",
-		access_token=jwt_token,
-		user=UserResponse(
-			id = user.id,
-			email = user.email,
-			username = user.username,
-			verified_at = user.verified_at,
-		)
-	)
-
-
-async def get_current_user(db:SessionDep, token: str = Depends(oauth2_scheme)) -> User:
-	token_timeout_exception = HTTPException(
-		status_code=status.HTTP_401_UNAUTHORIZED,
-		detail='Access token expired. Please generate new access token',
-		headers={"WWW-Authenticate": "Bearer"},
-	)
-	
-	credentials_exception = HTTPException(
-		status_code=status.HTTP_401_UNAUTHORIZED,
-		detail='Could not validate credentials',
-		headers={"WWW-Authenticate": "Bearer"},
-	)
-
-	user_not_found_exception = HTTPException(
-		status_code=status.HTTP_404_NOT_FOUND,
-		detail='User not found',
-	)
-
-	try:
-		jwt_payload = decode_access_token(token)
-		 
-		current_user_id = UUID(jwt_payload.get("sub"))
-		 
-		if current_user_id is None:
-			raise credentials_exception
-	
-	except ExpiredSignatureError:
-		raise token_timeout_exception
-
-	except JWTError:
-		raise credentials_exception
-
-	current_user_statement = select(User).where(
-		User.id == current_user_id)
-
-	result = db.execute(current_user_statement).scalar_one_or_none()
-
-	if result is None:
-		raise user_not_found_exception
-
-	return result
-
-
-async def verify_user_registration_token(db:Session, payload:VerifyRegistrationTokenRequest) -> VerifyRegistrationTokenResponse:
-
-	hashed_token = hash_verification_token(payload.verification_token)
-	
-	# Get the user from pending registrations table
-	statement = select(PendingRegistration).where(
-		PendingRegistration.token == hashed_token
-	)
-	
-	result = db.execute(statement)
-
-
-	pending_registration = result.scalar_one_or_none()
-
-	print(pending_registration.email)
-
-
-	# Check if token is valid
-	if(not pending_registration):
-		return VerifyRegistrationTokenResponse(
-			status="invalid",
-		)
-
-	# Check if token has expired
-	if datetime.now(timezone.utc) > pending_registration.expires_at:
-		return VerifyRegistrationTokenResponse(
-			status="expired",
-		)
-	else:
-		return VerifyRegistrationTokenResponse(
-			status="valid",
-		)
-
-
-
-async def register_user(db:Session, payload:RegisterRequest) -> RegisterResponse:
-	
-	# Check if user already registered
-	existing_user_statement = select(User).where(
-		User.email == payload.email)
-
-	existing_user_result = db.execute(existing_user_statement)
-
-	existing_user = existing_user_result.scalar_one_or_none()
-
-	if existing_user:
-		raise HTTPException(
-			status_code=status.HTTP_409_CONFLICT,
-			detail='An account already exists with this email.'
-		)
-		
-	# Get the user from pending registrations table
-	statement = select(PendingRegistration).where(
-		PendingRegistration.email == payload.email
-	)
-	
-	result = db.execute(statement)
 
-	pending_registration = result.scalar_one_or_none()
-
-	# Existing pending registration found
-	if pending_registration:
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/api/v1/auth/login"
+)
 
-		# If token is still valid
-		if pending_registration.expires_at > datetime.now(timezone.utc):
-			remaining_seconds = int((pending_registration.expires_at - datetime.now(timezone.utc)).total_seconds())
 
-			return RegisterResponse(
-				message="Verification email already sent",
-				expires_in=remaining_seconds,
-				can_resend=False,
-			)
-		# If token expired
-		expires_at = datetime.now(timezone.utc) + timedelta(minutes=5)
-		
-		token = generate_verification_token()
-		hashed_token = hash_verification_token(token)
-		
-		pending_registration.expires_at = expires_at
-		pending_registration.token = hashed_token
-		
-		db.commit()
-		
-		db.refresh(pending_registration)
+token_timeout_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Access token expired. Please generate new access token",
+    headers={"WWW-Authenticate": "Bearer"},
+)
 
-		verification_link = generate_verification_link(token)
 
-		await send_email_verification_mail(verification_link, payload.email)
-		
-		return RegisterResponse(
-			message="Previous verification expired. A new verification email will be sent.",
-			verification_token=token,
-			can_resend=True,
-			expires_in=300,
-		)
+credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+)
 
-	# No pending registration exists
-	expires_at = datetime.now(timezone.utc) + timedelta(minutes=5)
-	
-	token = generate_verification_token()
-	hashed_token = hash_verification_token(token)
 
-	new_pending_registration = PendingRegistration(
-		expires_at=expires_at,
-		email=payload.email,
-		token=hashed_token,
-	)
+user_not_found_exception = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND,
+    detail="User not found",
+)
 
-	db.add(new_pending_registration)
 
-	db.commit()
+authentication_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Invalid email or password",
+)
 
-	db.refresh(new_pending_registration)
 
-	verification_link = generate_verification_link(token)
+async def login(
+    db: Session,
+    payload: LoginRequest,
+) -> LoginResponse:
 
-	await send_email_verification_mail(verification_link, payload.email)
+    # Find user by email
+    find_user_statement = select(User).where(
+        User.email == payload.email
+    )
 
-	return RegisterResponse(
-		message="Verification email sent successfully",
-		verification_token=token,
-		can_resend=False,
-		expires_in=300,
-	)
+    result = db.execute(find_user_statement)
 
+    user = result.scalar_one_or_none()
 
+    # If user is not present
+    if not user:
+        raise authentication_exception
 
+    # If password verification failed
+    password_match = verify_password(
+        payload.password,
+        user.password_hash,
+    )
 
-async def delete_pending_registration(db:Session, payload:DeleteRequest) -> DeleteResponse:
-	statement = select(PendingRegistration).where(
-		PendingRegistration.email == payload.email
-	)
-	
-	result = db.execute(statement)
+    if not password_match:
+        raise authentication_exception
 
-	pending_registration = result.scalar_one_or_none()
+    # Generate JWT token, JTI and expiration
+    jwt_token, jti, expires_at = create_access_token(user.id)
 
-	# If email is present
-	if pending_registration:
+    # Create user's session
+    user_session = UserSession(
+        jti=jti,
+        user_id=user.id,
+        expires_at=expires_at,
+    )
 
-		db.delete(pending_registration)
+    db.add(user_session)
+    db.commit()
 
-		db.commit()
+    return LoginResponse(
+        token_type="bearer",
+        access_token=jwt_token,
+        user=UserResponse(
+            id=user.id,
+            email=user.email,
+            username=user.username,
+            verified_at=user.verified_at,
+        ),
+    )
 
-		return DeleteResponse(
-			message=f"User with email {payload.email} deleted successfully from pending registrations"
-		)
-	# If email is not present
-	return DeleteResponse(
-		message=f"No user with email {payload.email} found in pending registrations"
-	)
 
+async def get_current_session(
+    db: SessionDep,
+    token: str = Depends(oauth2_scheme),
+) -> UserSession:
 
+    try:
+        # Decode and validate JWT
+        jwt_payload = decode_access_token(token)
 
+        # Extract JTI
+        jti = jwt_payload.get("jti")
 
-async def verify_and_create_new_user(db:Session, payload:VerifyUserRequest) -> VerifyUserResponse:
-	
-	# Hash incoming raw token
-	hashed_token = hash_verification_token(payload.token)
+        if not jti:
+            raise credentials_exception
 
-	# Hash plain text password
-	hashed_password = hash_password(payload.password)
+        current_jti = UUID(jti)
 
-	# Find user in pending registrations
-	statement = select(PendingRegistration).where(
-		PendingRegistration.token == hashed_token)
+    except ExpiredSignatureError:
+        raise token_timeout_exception
 
-	result = db.execute(statement)
+    except (JWTError, ValueError):
+        raise credentials_exception
 
-	pending_registration = result.scalar_one_or_none()
+    # Find the session associated with this JWT
+    session_statement = select(UserSession).where(
+        UserSession.jti == current_jti
+    )
 
-	# Check if token is valid
-	if not pending_registration:
-		return VerifyUserResponse(
-				message="Invalid token."
-		)
+    user_session = db.execute(
+        session_statement
+    ).scalar_one_or_none()
 
-	# Check if token is expired
-	if datetime.now(timezone.utc) > pending_registration.expires_at:
-		return VerifyUserResponse(
-			message="Verification token expired."
-		)
+    # Session was deleted/revoked
+    if not user_session:
+        raise credentials_exception
 
-	# Create new user
-	new_user = User(
-		username=pending_registration.email,
-		email=pending_registration.email,
-		password_hash=hashed_password,
-	)
+    return user_session
 
-	db.add(new_user)
 
-	# Remove pending registration for user
-	db.delete(pending_registration)
+async def get_current_user(
+    db: SessionDep,
+    current_session: UserSession = Depends(get_current_session),
+) -> User:
 
-	# Add new user and commit to user table while removing from pending transaction
-	db.commit()
+    current_user_statement = select(User).where(
+        User.id == current_session.user_id
+    )
 
-	return VerifyUserResponse(
-		message="User created successfully."
-	)
+    result = db.execute(
+        current_user_statement
+    ).scalar_one_or_none()
+
+    if result is None:
+        raise user_not_found_exception
+
+    return result
+
+
+async def logout_user(
+    db: SessionDep,
+    current_session: UserSession,
+):
+    db.delete(current_session)
+    db.commit()
+
+
+async def verify_user_registration_token(
+    db: Session,
+    payload: VerifyRegistrationTokenRequest,
+) -> VerifyRegistrationTokenResponse:
+
+    hashed_token = hash_verification_token(
+        payload.verification_token
+    )
+
+    # Get the user from pending registrations table
+    statement = select(PendingRegistration).where(
+        PendingRegistration.token == hashed_token
+    )
+
+    result = db.execute(statement)
+
+    pending_registration = result.scalar_one_or_none()
+
+    print(pending_registration.email)
+
+    # Check if token is valid
+    if not pending_registration:
+        return VerifyRegistrationTokenResponse(
+            status="invalid",
+        )
+
+    # Check if token has expired
+    if datetime.now(timezone.utc) > pending_registration.expires_at:
+        return VerifyRegistrationTokenResponse(
+            status="expired",
+        )
+
+    return VerifyRegistrationTokenResponse(
+        status="valid",
+    )
+
+
+async def register_user(
+    db: Session,
+    payload: RegisterRequest,
+) -> RegisterResponse:
+
+    # Check if user already registered
+    existing_user_statement = select(User).where(
+        User.email == payload.email
+    )
+
+    existing_user_result = db.execute(
+        existing_user_statement
+    )
+
+    existing_user = existing_user_result.scalar_one_or_none()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="An account already exists with this email.",
+        )
+
+    # Get the user from pending registrations table
+    statement = select(PendingRegistration).where(
+        PendingRegistration.email == payload.email
+    )
+
+    result = db.execute(statement)
+
+    pending_registration = result.scalar_one_or_none()
+
+    # Existing pending registration found
+    if pending_registration:
+
+        # If token is still valid
+        if pending_registration.expires_at > datetime.now(timezone.utc):
+
+            remaining_seconds = int(
+                (
+                    pending_registration.expires_at
+                    - datetime.now(timezone.utc)
+                ).total_seconds()
+            )
+
+            return RegisterResponse(
+                message="Verification email already sent",
+                expires_in=remaining_seconds,
+                can_resend=False,
+            )
+
+        # If token expired
+        expires_at = (
+            datetime.now(timezone.utc)
+            + timedelta(minutes=5)
+        )
+
+        token = generate_verification_token()
+        hashed_token = hash_verification_token(token)
+
+        pending_registration.expires_at = expires_at
+        pending_registration.token = hashed_token
+
+        db.commit()
+
+        db.refresh(pending_registration)
+
+        verification_link = generate_verification_link(token)
+
+        await send_email_verification_mail(
+            verification_link,
+            payload.email,
+        )
+
+        return RegisterResponse(
+            message="Previous verification expired. A new verification email will be sent.",
+            verification_token=token,
+            can_resend=True,
+            expires_in=300,
+        )
+
+    # No pending registration exists
+    expires_at = (
+        datetime.now(timezone.utc)
+        + timedelta(minutes=5)
+    )
+
+    token = generate_verification_token()
+    hashed_token = hash_verification_token(token)
+
+    new_pending_registration = PendingRegistration(
+        expires_at=expires_at,
+        email=payload.email,
+        token=hashed_token,
+    )
+
+    db.add(new_pending_registration)
+
+    db.commit()
+
+    db.refresh(new_pending_registration)
+
+    verification_link = generate_verification_link(token)
+
+    await send_email_verification_mail(
+        verification_link,
+        payload.email,
+    )
+
+    return RegisterResponse(
+        message="Verification email sent successfully",
+        verification_token=token,
+        can_resend=False,
+        expires_in=300,
+    )
+
+
+async def delete_pending_registration(
+    db: Session,
+    payload: DeleteRequest,
+) -> DeleteResponse:
+
+    statement = select(PendingRegistration).where(
+        PendingRegistration.email == payload.email
+    )
+
+    result = db.execute(statement)
+
+    pending_registration = result.scalar_one_or_none()
+
+    if pending_registration:
+
+        db.delete(pending_registration)
+
+        db.commit()
+
+        return DeleteResponse(
+            message=(
+                f"User with email {payload.email} "
+                "deleted successfully from pending registrations"
+            )
+        )
+
+    return DeleteResponse(
+        message=(
+            f"No user with email {payload.email} "
+            "found in pending registrations"
+        )
+    )
+
+
+async def verify_and_create_new_user(
+    db: Session,
+    payload: VerifyUserRequest,
+) -> VerifyUserResponse:
+
+    # Hash incoming raw token
+    hashed_token = hash_verification_token(
+        payload.token
+    )
+
+    # Hash plain text password
+    hashed_password = hash_password(
+        payload.password
+    )
+
+    # Find user in pending registrations
+    statement = select(PendingRegistration).where(
+        PendingRegistration.token == hashed_token
+    )
+
+    result = db.execute(statement)
+
+    pending_registration = result.scalar_one_or_none()
+
+    # Check if token is valid
+    if not pending_registration:
+        return VerifyUserResponse(
+            message="Invalid token."
+        )
+
+    # Check if token is expired
+    if datetime.now(timezone.utc) > pending_registration.expires_at:
+        return VerifyUserResponse(
+            message="Verification token expired."
+        )
+
+    # Create new user
+    new_user = User(
+        username=pending_registration.email,
+        email=pending_registration.email,
+        password_hash=hashed_password,
+    )
+
+    db.add(new_user)
+
+    # Remove pending registration
+    db.delete(pending_registration)
+
+    # Commit user creation and pending registration removal
+    db.commit()
+
+    return VerifyUserResponse(
+        message="User created successfully."
+    )
