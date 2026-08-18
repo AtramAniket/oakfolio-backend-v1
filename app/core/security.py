19f6a3a6a688c2980658bca45f0ad90ae040d7f3
AtramAniket <aniket_atram@live.in>
2026-08-18 15:13:25 +0530

    Update the access token method

--- app/core/security.py
+++ app/core/security.py
@@ -1,8 +1,8 @@
 import hashlib
 import secrets
 from jose import jwt
-from uuid import UUID
 from typing import Any
+from uuid import UUID, uuid4
 from app.core.config import settings
 from passlib.context import CryptContext
 from datetime import datetime, timezone, timedelta
@@ -13,21 +13,33 @@ pwd_context = CryptContext(
 	deprecated="auto",
 )
 
-def create_access_token(user_id: UUID) -> str:
-	"""Function generates JWT access token for user id"""
-	token_expires_at = datetime.now(timezone.utc) + timedelta(minutes=int(settings.access_token_expire_minutes))
 
-	payload: dict[str, Any] = {
-		"sub": str(user_id),
-		"exp": token_expires_at,
-		"iat": datetime.now(timezone.utc)
-	}
+def create_access_token(user_id: UUID) -> tuple[str, UUID, datetime]:
+    now = datetime.now(timezone.utc)
 
-	return jwt.encode(
-		payload,
-		settings.secret_key,
-		algorithm=settings.algorithm,
-	)
+    token_expires_at = (
+        now + timedelta(
+            minutes=int(settings.access_token_expire_minutes)
+        )
+    )
+
+    jti = uuid4()
+
+    payload: dict[str, Any] = {
+        "sub": str(user_id),
+        "jti": str(jti),
+        "exp": token_expires_at,
+        "iat": now,
+    }
+
+    token = jwt.encode(
+        payload,
+        settings.secret_key,
+        algorithm=settings.algorithm,
+    )
+
+    return token, jti, token_expires_at
+
 
 def decode_access_token(token: str) -> dict[str, Any]:
 	"""Function returns user's id if the token is valid"""
