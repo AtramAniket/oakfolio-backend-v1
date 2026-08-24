febd5df2f14cfa597b0351216657425e3198772a
AtramAniket <aniket_atram@live.in>
2026-08-24 16:32:42 +0530

    Added activities router

--- app/api/v1/api_router.py
+++ app/api/v1/api_router.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from app.modules.auth.router import auth_router
 from app.modules.stocks.routes import api_v1_stocks_router
+from app.modules.activity.routes import api_v1_activities_router
 
 api_v1_router = APIRouter()
 
 api_v1_router.include_router(auth_router)
 api_v1_router.include_router(api_v1_stocks_router)
+api_v1_router.include_router(api_v1_activities_router)
